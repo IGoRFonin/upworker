9dc4a9b770f4a1eeae2d4a8e57a66c13dfa80593
IGoRFonin <ifonin@dalee.ru>
2018-02-08 09:42:01 +0300

    job-data-initial

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const jobSelectors = {
     title: '[data-job-title] a',
     location: '.client-location',
     spent: '.client-spendings strong',
-    skills: '.o-tag-skill'
+    skills: '.o-tag-skill span',
+    paidInfo: 'small.text-muted.display-inline-block.m-sm-top'
 };
 
 
@@ -26,13 +27,12 @@ const jobSelectors = {
                 button.click();
             }
         });
-        console.log(submitSelector);
         return Promise.resolve();
     });
-    await page.waitFor(300);
-
-
-    console.log('page open')
+    await page.waitFor(500);
+    console.log('page open');
+    const jobData = await page.evaluate(frontGetData, frontSelectors);
+    console.log(jobData);
     // await browser.close();
 })();
 
@@ -45,11 +45,37 @@ const frontGetData = frontSelectors => {
     const jobs = document.querySelectorAll(jobsSelector);
 
     const data = [].slice.call(jobs).map(parseJobs);
+
+    return Promise.resolve(data);
 }
 
 const parseJobs = section => {
     const job = {};
     job.title = document.querySelector(frontSelectors.title).textContent;
+    job.link = document.querySelector(frontSelectors.title).href;
+    job.id = getId(job.link);
     job.location = document.querySelector(frontSelectors.location).textContent;
     job.spent = document.querySelector(frontSelectors.spent).textContent;
+    job.skills = getSkills(document.querySelectorAll(frontSelectors.skills));
+    job.paidInfo = document.querySelector(frontSelectors.paidInfo).textContent.replace(/\s+/g, ' ').trim();
+    return job;
 }
+
+const getId = href => {
+    const clip = href.substr(1, href.length - 2);
+    const clipList = clip.split('/');
+    return clipList[clipList.length - 1];
+}
+
+const getSkills = nodes => [].slice.call(nodes).map(node => node.textContent);
+
+/**
+ * job {Object}
+ * job.id {String}
+ * job.title {String}
+ * job.link {String}
+ * job.location {String}
+ * job.spent {String}
+ * job.skills {Object[]}
+ * job.paidInfo {String}
+ */
